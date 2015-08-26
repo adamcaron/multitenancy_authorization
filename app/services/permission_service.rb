d7f403bce540c9f372412e23ff43ae084fcdca35
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-26 09:50:55 -0600

    add permission servoces

---
+++ app/services/permission_service.rb
@@ -0,0 +1,10 @@
+class PermissionService
+  attr_reader :user
+  def initialize(user)
+    @user = user
+  end
+
+  def allow?(controller, action)
+    return true if controller == 'stores' && action == 'index'
+  end
+end
