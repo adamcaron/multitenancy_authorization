0dace109c7b64497dab904ff565c7e5646077808
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-26 10:17:28 -0600

    add conditional to give reg users access

--- app/services/permission_service.rb
+++ app/services/permission_service.rb
@@ -1,10 +1,16 @@
 class PermissionService
   attr_reader :user
   def initialize(user)
-    @user = user
+    @user = user || User.new
   end
 
   def allow?(controller, action)
-    return true if controller == 'stores' && action == 'index'
+    if user.registered_user?
+      return true if controller == 'stores' && action.in?(%w(index show))
+      return true if controller == 'sessions' && action.in?(%w(new create destroy))
+    else
+      return true if controller == 'stores' && action == 'index'
+      return true if controller == 'sessions' && action.in?(%(new create destroy))
+    end
   end
 end
