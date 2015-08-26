34f38d9d01ce68febb7d720f1fdd31962e87ae33
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-26 10:35:36 -0600

    refactor #authorize in app ctrler, #allow? in permission services

--- app/services/permission_service.rb
+++ app/services/permission_service.rb
@@ -1,16 +1,29 @@
 class PermissionService
-  attr_reader :user
+  attr_reader :user, :controller, :action
   def initialize(user)
     @user = user || User.new
   end
 
   def allow?(controller, action)
+    @controller = controller
+    @action     = action
+
     if user.registered_user?
-      return true if controller == 'stores' && action.in?(%w(index show))
-      return true if controller == 'sessions' && action.in?(%w(new create destroy))
+      registered_user_permissions
     else
-      return true if controller == 'stores' && action == 'index'
-      return true if controller == 'sessions' && action.in?(%(new create destroy))
+      guest_user_permissions
     end
+  end
+
+  private
+
+  def registered_user_permissions
+    return true if controller == 'stores' && action.in?(%w(index show))
+    return true if controller == 'sessions' && action.in?(%w(new create destroy))
+  end
+
+  def guest_user_permissions
+    return true if controller == 'stores' && action == 'index'
+    return true if controller == 'sessions' && action.in?(%(new create destroy))
   end
 end
